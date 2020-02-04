d490ba69459ac411da64713e3c0899a3767407b3
Hitesh Kumar <hitesh.kumar15@imperial.ac.uk>
2020-02-04 15:23:42 +0000

    Moved to_numpy() to common

--- distributions.py
+++ distributions.py
@@ -1,9 +1,7 @@
 # Imports
-import collections
+import common
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import torch
 import scipy.stats as scistat
 
 
@@ -43,7 +41,7 @@ def plot_distributions(variables, names=[], optimise_bins=True):
     """
     
     # Standardise data type
-    variables = to_numpy(variables)
+    variables = common.to_numpy(variables)
     num_variables = variables.shape[0]
         
     # Check for and alert about NaNs
@@ -117,7 +115,7 @@ def clip_distribution(variables):
     """
     
     # Standardise data type
-    variables = to_numpy(variables)
+    variables = common.to_numpy(variables)
     num_variables = variables.shape[0]
         
     # Determine the size of the clipped data (all rows will have same number 
@@ -141,42 +139,3 @@ def clip_distribution(variables):
         clipped_variables[i, :] = variables[i, ~extreme_loc]
 
     return clipped_variables
-
-
-def to_numpy(data):
-    """
-    Make sure that data entered into any function is compatible shape and type.
-    
-    Parameters
-    ----------
-    data: array-like
-        Some data form in array like structures, could be:
-            Numpy array
-            Pandas DataFrame/Series
-            Torch Tensor
-            List, tuple, deque
-    
-    Returns
-    -------
-    variables: Numpy array
-        Compatible and usable object which can be passed into any function in 
-        this codebase, shaped as a 2D array
-    """
-    
-    # Standardise data type
-    acceptable_types = [list, tuple, collections.deque]
-    if any(type(data) == obj for obj in acceptable_types):
-        variables = np.array(data)
-    elif type(data) == pd.Series or type(data) == pd.DataFrame:
-        variables = data.to_numpy()
-    elif type(data) == torch.Tensor:
-        variables = data.numpy()
-    elif type(data) != np.ndarray:
-        raise TypeError('Input is not an acceptable array-like iterable object')
-        return None
-        
-    # If not 2D, make it 2D
-    if len(variables.shape) < 2:
-        variables = variables.reshape(1, -1)
-        
-    return variables
